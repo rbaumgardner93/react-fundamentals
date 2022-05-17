4a4593645670e8e09bc01d54ba2f2533416a41e2
Ryan Baumgardner <ryan.baumgardner@protonmail.com>
2022-05-17 14:28:15 -0400

    feat: complete exercise 6

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -19,13 +19,29 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  const [ username, setUsername ] = React.useState( '' );
+
+  const handleSubmit = ( e ) => {
+		e.preventDefault();
+
+	    onSubmitUsername( username );
+  };
+
+  const handleChange = e => {
+	  setUsername( e.target.value.toLowerCase() );
+  }
+
   return (
-    <form>
+	  <form onSubmit={ handleSubmit }>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="usernameInput">Username:</label>
+	  	<input
+	  		value={ username }
+	  		id="usernameInput"
+	  		type="text"
+	  		onChange={ handleChange } />
       </div>
-      <button type="submit">Submit</button>
+	  <button type="submit">Submit</button>
     </form>
   )
 }
